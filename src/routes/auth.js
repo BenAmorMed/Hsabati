0ba59142a00ec23e99145b41444e40b0dc937aab
BenAmorMed <medbenamor1919@gmail.com>
2026-02-20 21:03:23 +0100

    refactor: remove legacy express codebase
    Deleted old src directory after successful migration to NestJS monorepo

--- src/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const protect = require('../middleware/auth');
-const { signup, login, logout, refresh, signupRules, loginRules } = require('../controllers/authController');
-
-router.post('/signup', signupRules, signup);
-router.post('/login', loginRules, login);
-router.post('/logout', protect, logout);
-router.post('/refresh', refresh);
-
-module.exports = router;
